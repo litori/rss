37438e560ebfe9875fcb505a5aaa2d8a0a4b3711
litori <sonontse@gmail.com>
2023-08-17 03:41:37 -0700

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full dark:bg-gray-900">
 <head>
+
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-74743832-1"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-TD89GDMG1W"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-    
-      gtag('config', 'UA-74743832-1');
+
+      gtag('config', 'G-TD89GDMG1W');
     </script>
     <meta charset="utf-8">
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6656872145113265"
